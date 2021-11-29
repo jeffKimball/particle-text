db6f056f1c239f7ab373abf993ad07ba489ff87e
Jeff Kimball <jeffkimballis@gmail.com>
2021-11-28 18:52:14 -0600

    complete particle text

--- main.js
+++ main.js
@@ -4,35 +4,38 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 let particleArray = []
 
+//shortcut method to postioning text
+let adjustX = 33.5
+let adjustY = 4
+
 //handle mouse
 const mouse = {
     x: null,
     y: null,
-    radius: 150
+    radius: 120
 }
 
 window.addEventListener('mousemove', (event) => {
     mouse.x = event.x
     mouse.y = event.y
-    // console.log(mouse.x, mouse.y);
 })
 
 ctx.fillStyle = 'white'
-ctx.font = '30px Verdana'
-ctx.fillText('A', 0, 30)
-const data = ctx.getImageData(0, 0, 100, 100)
+ctx.font = '25px Verdana'
+ctx.fillText('{code}', 0, 40) //controls margin top
+const textCoordinates = ctx.getImageData(0, 0, 100, 100)//controls text area size and location
 
 class Particle {
     constructor(x, y){
         this.x = x
         this.y = y
-        this.size = 3
+        this.size = 2
         this.baseX = this.x
         this.baseY = this.y
-        this.density = (Math.random() * 30) + 1
+        this.density = (Math.random() * 40) + 5
     }
     draw(){
-        ctx.fillStyle = 'red'
+        ctx.fillStyle = 'white'
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
         ctx.closePath()
@@ -42,20 +45,39 @@ class Particle {
         let dx = mouse.x - this.x
         let dy = mouse.y - this.y
         let distance = Math.sqrt(dx * dx + dy * dy)
-        if(distance < 100){
-            this.size = 20
+        let forceDirectionX = dx / distance
+        let forceDirectionY = dy / distance
+        let maxDistance = mouse.radius
+        let force = (maxDistance - distance) / maxDistance
+        let directionX = forceDirectionX * force * this.density
+        let directionY = forceDirectionY * force * this.density
+
+        if(distance < mouse.radius){
+            this.x -= directionX 
+            this.y -= directionY
         }else{
-            this.size = 3
+            if(this.x !== this.baseX){
+                let dx = this.x - this.baseX
+                this.x -= dx/10
+            }
+            if(this.y !== this.baseY){
+                let dy = this.y - this.baseY
+                this.y -= dy/10
+            }
         }
     }
 }
-
+console.log(textCoordinates);
 function init(){
     particleArray = []
-    for(let i = 0; i <  1000; i++){
-        let x = Math.random() * canvas.width
-        let y = Math.random() * canvas.height 
-        particleArray.push(new Particle(x, y))
+    for(let y = 0, y2 = textCoordinates.height; y < y2; y++){
+        for(let x = 0, x2 = textCoordinates.width; x < x2; x++){
+            if(textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) +3] > 128){
+                let positionX = x + adjustX
+                let positionY = y + adjustY
+                particleArray.push(new Particle(positionX * 10, positionY * 10))//control size of particle spread
+            }
+        }
     }
 }
 init()
@@ -67,6 +89,28 @@ function animate(){
         particleArray[i].draw()
         particleArray[i].update()
     }
+    connect()
     requestAnimationFrame(animate)
 }
 animate()
+
+function connect(){
+    let opacityValue = 1
+    for(let a = 0; a < particleArray.length; a++){
+        for(let b = a; b < particleArray.length; b++){
+            let dx = particleArray[a].x - particleArray[b].x
+            let dy = particleArray[a].y - particleArray[b].y
+            let distance = Math.sqrt(dx * dx + dy * dy)
+
+            if(distance < 22){  
+                opacityValue = 1 -(distance/22) // keep number same as line 105
+                ctx.strokeStyle = 'rgba(255,255,255,' + opacityValue + ')'              
+                ctx.lineWidth = 2
+                ctx.beginPath()
+                ctx.moveTo(particleArray[a].x, particleArray[a].y)
+                ctx.lineTo(particleArray[b].x, particleArray[b].y)
+                ctx.stroke()
+            }
+        }
+    }
+}
